22a3d5bcf0333e6d214cd97531b9b7ec0cfd36ad
Hagen Timper <hagen.timper@gmx.de>
2017-07-07 11:44:55 +0200

    Add files via upload

---
+++ MyChat/MyChat/MainForm.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace MyChat
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbIpQuelle = new System.Windows.Forms.TextBox();
+            this.tbPortQuelle = new System.Windows.Forms.TextBox();
+            this.tbPortZiel = new System.Windows.Forms.TextBox();
+            this.tbIpZiel = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lbChatFenster = new System.Windows.Forms.ListBox();
+            this.tbNachricht = new System.Windows.Forms.TextBox();
+            this.btnSenden = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Port :";
+            // 
+            // tbIpQuelle
+            // 
+            this.tbIpQuelle.Location = new System.Drawing.Point(46, 12);
+            this.tbIpQuelle.Name = "tbIpQuelle";
+            this.tbIpQuelle.Size = new System.Drawing.Size(126, 20);
+            this.tbIpQuelle.TabIndex = 2;
+            // 
+            // tbPortQuelle
+            // 
+            this.tbPortQuelle.Location = new System.Drawing.Point(46, 38);
+            this.tbPortQuelle.Name = "tbPortQuelle";
+            this.tbPortQuelle.Size = new System.Drawing.Size(126, 20);
+            this.tbPortQuelle.TabIndex = 3;
+            // 
+            // tbPortZiel
+            // 
+            this.tbPortZiel.Location = new System.Drawing.Point(239, 38);
+            this.tbPortZiel.Name = "tbPortZiel";
+            this.tbPortZiel.Size = new System.Drawing.Size(126, 20);
+            this.tbPortZiel.TabIndex = 7;
+            // 
+            // tbIpZiel
+            // 
+            this.tbIpZiel.Location = new System.Drawing.Point(239, 12);
+            this.tbIpZiel.Name = "tbIpZiel";
+            this.tbIpZiel.Size = new System.Drawing.Size(126, 20);
+            this.tbIpZiel.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(201, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Port :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(201, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "IP :";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(371, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 46);
+            this.btnStart.TabIndex = 8;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lbChatFenster
+            // 
+            this.lbChatFenster.FormattingEnabled = true;
+            this.lbChatFenster.Location = new System.Drawing.Point(11, 64);
+            this.lbChatFenster.Name = "lbChatFenster";
+            this.lbChatFenster.Size = new System.Drawing.Size(434, 277);
+            this.lbChatFenster.TabIndex = 9;
+            // 
+            // tbNachricht
+            // 
+            this.tbNachricht.Location = new System.Drawing.Point(11, 349);
+            this.tbNachricht.Name = "tbNachricht";
+            this.tbNachricht.Size = new System.Drawing.Size(353, 20);
+            this.tbNachricht.TabIndex = 10;
+            // 
+            // btnSenden
+            // 
+            this.btnSenden.Location = new System.Drawing.Point(370, 347);
+            this.btnSenden.Name = "btnSenden";
+            this.btnSenden.Size = new System.Drawing.Size(75, 23);
+            this.btnSenden.TabIndex = 11;
+            this.btnSenden.Text = "Senden";
+            this.btnSenden.UseVisualStyleBackColor = true;
+            this.btnSenden.Click += new System.EventHandler(this.btnSenden_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 383);
+            this.Controls.Add(this.btnSenden);
+            this.Controls.Add(this.tbNachricht);
+            this.Controls.Add(this.lbChatFenster);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.tbPortZiel);
+            this.Controls.Add(this.tbIpZiel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbPortQuelle);
+            this.Controls.Add(this.tbIpQuelle);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbIpQuelle;
+        private System.Windows.Forms.TextBox tbPortQuelle;
+        private System.Windows.Forms.TextBox tbPortZiel;
+        private System.Windows.Forms.TextBox tbIpZiel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lbChatFenster;
+        private System.Windows.Forms.TextBox tbNachricht;
+        private System.Windows.Forms.Button btnSenden;
+    }
+}
+
---
+++ MyChat/MyChat/MainForm.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace MyChat
+{
+    public partial class MainForm : Form
+    {
+        Socket socket;
+        EndPoint epLocal, epRemote;
+        byte[] buffer;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            tbIpQuelle.Text = GetIP();
+        }
+
+        private string GetIP()
+        {
+            IPHostEntry host;
+            host = Dns.GetHostEntry(Dns.GetHostName());
+
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+
+            return "127.0.0.1";
+        }
+
+        private void MessageCallBack(IAsyncResult aResult)
+        {
+            byte[] recData = new byte[1500];
+            recData = (byte[])aResult.AsyncState;
+            ASCIIEncoding aEncoding = new ASCIIEncoding();
+            string recMessage = aEncoding.GetString(recData);
+
+            lbChatFenster.Items.Add("Friend: " + recMessage);
+
+            buffer = new byte[1500];
+            socket.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref epRemote, new AsyncCallback(MessageCallBack), buffer);
+        }
+
+        private void btnSenden_Click(object sender, EventArgs e)
+        {
+            ASCIIEncoding aEncoding = new ASCIIEncoding();
+            byte[] sendM = new byte[1500];
+            sendM = aEncoding.GetBytes(tbNachricht.Text);
+            socket.Send(sendM);
+
+            lbChatFenster.Items.Add("Me: " + tbNachricht.Text);
+            tbNachricht.Clear();
+            tbNachricht.Focus();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            epLocal = new IPEndPoint(IPAddress.Parse(tbIpQuelle.Text), Convert.ToInt32(tbPortQuelle.Text));
+            socket.Bind(epLocal);
+            epRemote = new IPEndPoint(IPAddress.Parse(tbIpZiel.Text), Convert.ToInt32(tbPortZiel.Text));
+            socket.Connect(epRemote);
+
+            buffer = new byte[1500];
+            socket.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref epRemote, new AsyncCallback(MessageCallBack), buffer);
+
+            tbNachricht.Focus();
+        }
+    }
+}
